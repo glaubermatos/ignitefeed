6e6ae58ca94f6eeacd8d618ae69e09cd54ee2164
glaubermatos <glaub.oliveira@hotmail.com>
2023-01-19 13:36:28 -0300

    create project, Header and Sidebar component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Glauber Matos"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque nobis asperiores ipsa natus quibusdam nisi ipsam iusto facere, eius eos reiciendis, fugit ratione aspernatur? Dolorem neque nisi unde id delectus!"
+          />
+          
+          <Post
+            author="Aijalon Brito"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque nobis asperiores ipsa natus quibusdam nisi ipsam iusto facere, eius eos reiciendis, fugit ratione aspernatur? Dolorem neque nisi unde id delectus!"
+          />
+        </main>
+      </div>
+
+    </div>
+    
+  )
+}
